14618a8d081a563bf8d972f0c3a826ae017e4817
Brandon Buck <lordizuriel@gmail.com>
2013-09-12 16:52:38 -0500

    Add files that should have been picked up a long time ago.

---
+++ lib/lang/runtime/class_instance.rb
@@ -0,0 +1,72 @@
+require "lang/runtime/class_skeleton"
+
+module Cuby
+  class CubyClassInstance < CubyClassSkeleton
+    attr_accessor :instance_vars, :runtime_class
+    set_is_instance
+
+    def call(method_name, arguments = [])
+      method = @runtime_class.instance_lookup(method_name.to_s)
+      if method.kind_of?(CubyMethod)
+        method.call(self, arguments, runtime_class.memory)
+      else
+        method.call(self, arguments)
+      end
+    end
+
+    def initialize(memory, runtime_class)
+      @instance_vars = {}
+      @memory = memory
+      @runtime_class = runtime_class
+      @ruby_value = self
+    end
+
+    def to_s
+      "<CubyClassInstance @instance_of=\"#{runtime_class.name || "Unnamed"}\">"
+    end
+
+    def inspect
+      to_s
+    end
+
+    def is_a?(value)
+      names = ["Object", runtime_class.name]
+      cur_class = runtime_class
+      while cur_class.super_class.name != "Object"
+        names << cur_class.super_class.name
+        cur_class = cur_class.super_class
+      end
+      names.include?(value)
+    end
+
+    def class_name
+      @runtime_class.name
+    end
+
+    def eql?(o)
+      p self
+      p o
+      if o.kind_of?(CubyClassInstance)
+        if literal_type?
+          ruby_value == o.ruby_value
+        else
+          object_id == o.object_id
+        end
+      else
+        false
+      end
+    end
+
+    def hash
+      if literal_type?
+        ruby_value.hash
+      else
+        call(:to_string).ruby_value.hash
+      end
+    end
+
+    def literal_type?
+      LITERAL_TYPES.include?(class_name)
+    end
+  end
+end
---
+++ lib/lang/runtime/class_skeleton.rb
@@ -0,0 +1,36 @@
+module Cuby
+  class CubyClassSkeleton
+    attr_accessor :ruby_value
+    attr_reader :memory
+
+    class << self
+      def set_is_instance
+        self.class_eval do
+          def instance?
+            true
+          end
+        end
+      end
+
+      def set_is_class
+        self.class_eval do
+          def class?
+            true
+          end
+        end
+      end
+    end
+
+    def instance?
+      false
+    end
+
+    def class?
+      false
+    end
+
+    def is_a?(name)
+      false
+    end
+  end
+end
