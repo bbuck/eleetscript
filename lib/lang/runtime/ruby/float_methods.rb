a5b28516740e8e4cde3089b044d869ed6c39d786
Brandon <lordizuriel@gmail.com>
2014-12-13 02:16:01 -0600

    Add rounding methods to integers and add floor, ceil methods.

--- lib/lang/runtime/ruby/float_methods.rb
+++ lib/lang/runtime/ruby/float_methods.rb
@@ -14,10 +14,18 @@ module EleetScript
       if arguments.length == 0
         root_namespace["Integer"].new_with_value(receiver.ruby_value.round, context.namespace_context)
       elsif arguments.length >= 1 && arguments.first.is_a?("String", "Number")
-        root_namespace["Float"].new_with_value(receiver.ruby_value.round(arguments.first.calL(:to_integer).ruby_value), context.namespace_context)
+        root_namespace["Float"].new_with_value(receiver.ruby_value.round(arguments.first.call(:to_integer).ruby_value), context.namespace_context)
       else
         root_namespace["nil"]
       end
+    end
+
+    float.def :floor do |receiver, arguments, context|
+      root_namespace["Integer"].new_with_value(receiver.ruby_value.floor, context.namespace_context)
+    end
+
+    float.def :ceil do |receiver, arugments, context|
+      root_namespace["Integer"].new_with_value(receiver.ruby_value.ceil, context.namespace_context)
     end
   end
 end
